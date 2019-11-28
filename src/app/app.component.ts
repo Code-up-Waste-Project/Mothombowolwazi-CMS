311ddb7a80e103a0ab1acee5292ef04c23142be6
mmatliThato <hopethato69@gmail.com>
2019-11-28 08:21:32 +0200

    changes made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,6 +31,16 @@ export class AppComponent {
       url: '/analytics',
       icon: 'analytics'
     },
+    {
+      title: 'Login',
+      url: '/login',
+      icon: 'login'
+    },
+    {
+      title: 'Signin',
+      url: '/signin',
+      icon: 'signin'
+    },
   ];
 
   constructor(
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,10 @@
+
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../app/user/auth.service';
+import { LoadingController, AlertController, Platform } from '@ionic/angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
@@ -6,8 +12,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-
-  constructor() { }
+  public signupForm: FormGroup;
+  public loading: any;
+  constructor() 
+  { 
+    
+  }
 
   ngOnInit() {
   }
